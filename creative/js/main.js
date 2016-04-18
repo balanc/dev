af3c642da05ff9a3b5be2286e0e6363edff3ceb3
Christian James Balan <balanc@colorado.edu>
2016-04-17 22:10:09 -0600

    added a timer, game over phase, retry button, and invisible feature

--- creative/js/main.js
+++ creative/js/main.js
@@ -5,89 +5,110 @@ $( document ).ready(function() {
     var scoreBox = $("#score");
     var body = $("main");
     var targets = [];
-    var screenWidth = $(window).width()-100;
-    var screenHeight = $(window).height()-100;
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
     var targetCount = 0;
     var check = true;
     var newTarget;
+
+    var time = 5;
+    var cursorTime = 2;
     var cssleft = [];
     var csstop = [];
 
-	// $(body).hover(function() {
-	// 	$(this).css({
-	// 		'background': 'red'})
-	// });
+    var positionX;
+    var positionY;
+
 
 	function createTarget() {
 
 		var newTarget = $("<div class=target></div>"); //create DOM Element
-		// positionTarget(newTarget);
-        var positionX = Math.round(Math.random() * (screenWidth - 100)) + "px";
-        var positionY = Math.round(Math.random() * (screenHeight - 100)) + "px";
+
+		positionTarget(newTarget);
+
         console.log(positionX+" "+positionY);
 
-		// $(".target").css({
-		// 	"position": "absolute",
-		// 	"left": positionX,
-		// 	"top": positionY
-		// });
 
-		newTarget.css(
-			{	"position": "absolute",
-				"left": positionX,
-				"top": positionY
-			}
-		);
+		newTarget.css({	
+			"position": "absolute",
+			"left": positionX,
+			"top": positionY
+		});
 
 
-		newTarget.click(
-			function() { $(this).remove(); console.log("working?"); }
-		);
+		newTarget.click(function() { 
+				$(this).remove(); 
+				console.log("working?"); 
+				time = 5;
+		});
 		
 		targets.push(newTarget);
 
 		$("#field").append(newTarget);
 
-		console.log(targets)
+		newTarget.animate({
+			opacity: 0
+		}, 500);
+
+		// console.log(targets)
 
 		var n = $("div").length;
 
-		// for (i=0; i<n; i++) {
-		// 	var value = $(".target").css("left");
-		// 	css.push(value);
-		// 	console.log(css);
-		// }
-			var left = $(".target").css("left");
-			var top = $(".target").css("top");
-			cssleft.push(left);
-			csstop.push(top);
-			console.log("left:"+cssleft+" top:"+csstop);
 
-		console.log(n);
-	}
+		var left = $(".target").css("left");
+		var top = $(".target").css("top");
+		cssleft.push(left);
+		csstop.push(top);
+		// console.log("left:"+cssleft+" top:"+csstop);
+
+	};
 
 	function positionTarget(newTarget) {
-		// var random = Math.floor(Math.random()*500); //random number for positioning
-  //       var positionX = Math.round(Math.random() * screenWidth)-20;
-  //       var positionY = Math.round(Math.random() * screenHeight)-20;
-  //       console.log(positionX+" "+positionY);
-
-		// $(".target").css({
-		// 	"position": "absolute",
-		// 	"left": "positionX" + "px",
-		// 	"top": "positionY" + "px"
-		// });
-	}
+        positionX = Math.round(Math.random() * (screenWidth - 100)) + "px";
+        positionY = Math.round(Math.random() * (screenHeight - 100)) + "px";
+	};
 
 	// $(body).hover(function() {
 	// 	createTarget();
 	// });
 
-	var myVar = setInterval(createTarget, 1000)
 
-	$("#score").click(function() {
-		$(this).remove();
-		console.log("working?")
+	function countdown() {
+		time = time-1;
+		console.log("countdown time: " +time);
+		if (time <= 0) {
+			clearInterval(timer);
+			alert("times up, fool");
+			clearInterval(target);
+			$('div').remove();
+			$('body').css("cursor", "default");
+		};
+	};
+
+	function cursorInvisible() {
+
+		cursorTime = cursorTime-1;
+		console.log("cursorTime: " + cursorTime);
+
+		if(cursorTime == 0) {
+			$('body').css("cursor", "none");
+			console.log("Finally invisible?");
+			clearInterval(cursorInvisible);
+		};
+
+	};
+
+	$('button').click(function() {
+		time = 5;
+		cursorTime = 2;
+		timer = setInterval(countdown, 1000);
+		target = setInterval(createTarget, 1000);
+		cursor = setInterval(cursorInvisible, 1000);
 	});
+
+
+	var timer = setInterval(countdown, 1000);
+	var target = setInterval(createTarget, 1000);
+	var cursor = setInterval(cursorInvisible, 1000);
 
 });
